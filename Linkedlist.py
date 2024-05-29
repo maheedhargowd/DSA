babd7c70541c09ee83f7ce511141c15a7f2814e4
unknown <maheedhar gowd>
2024-05-29 08:55:47 +0530

    linkedlist class add_node function updated

--- Linkedlist.py
+++ Linkedlist.py
@@ -1,5 +1,29 @@
-# class linkedlist:
-#     def node(self):
+class Node:
+    def __init__(self,value,next=None):
+        self.value=value
+        self.next=next
 
-print("updated  linkedlist file")
+class LinkedList : 
+    def __init__(self,head=None):
+        self.head=head
         
+    def add_node (self,node):
+        if self.head is None : 
+            self.head = node
+        else : 
+            counter = self.head 
+            while counter.next:
+                counter =counter.next
+            counter.next=node
+
+l=LinkedList()
+a=Node(1)
+b=Node(5)
+l.add_node(a)
+l.add_node(b)
+
+print(l)
+current =l.head
+while current :
+    print(current.value)
+    current=current.next
